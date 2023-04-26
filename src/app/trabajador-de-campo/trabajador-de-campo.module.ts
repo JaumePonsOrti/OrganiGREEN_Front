8a66de1285ad8a486cfa3e749b054b394770ed91
Jaume <jaumeponsorti@gmail.com>
2023-04-26 11:54:25 +0200

    Trabajador de campo modulo

---
+++ src/app/trabajador-de-campo/trabajador-de-campo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TrabajadorDeCampoRoutingModule } from './trabajador-de-campo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TrabajadorDeCampoRoutingModule
+  ]
+})
+export class TrabajadorDeCampoModule { }
