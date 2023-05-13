6e7008b5808f26f388f79edce0731fc7ed8deb25
Jaume Pons Orti <jaumeponsorti@gmail.com>
2023-05-13 12:28:39 +0200

    Intranet añadido

---
+++ src/app/intranet/intranet.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IntranetRoutingModule } from './intranet-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IntranetRoutingModule
+  ]
+})
+export class IntranetModule { }
