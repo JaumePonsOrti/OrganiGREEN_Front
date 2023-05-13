82d453d95b51cd83f5f688b8658decd2216fb469
Jaume Pons Orti <jaumeponsorti@gmail.com>
2023-05-13 12:28:06 +0200

    ruta planificacion añadida

---
+++ src/app/core/shared/views/planificacion/planificacion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planificacion',
+  templateUrl: './planificacion.component.html',
+  styleUrls: ['./planificacion.component.scss'],
+})
+export class PlanificacionComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/core/shared/views/planificacion/planificacion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlanificacionRoutingModule } from './planificacion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PlanificacionRoutingModule
+  ]
+})
+export class PlanificacionModule { }
