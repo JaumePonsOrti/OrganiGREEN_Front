d6982ed56765ee7d1dfdbe0ce02b7b03c8e0ec21
Jaume Pons Orti <jaumeponsorti@gmail.com>
2023-05-23 09:56:41 +0200

    Eliminar view component y module modificado

--- src/app/core/shared/views/views.module.ts
+++ src/app/core/shared/views/views.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlanificacionModule } from './planificacion/planificacion.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, 
+    PlanificacionModule
+  ],
+  exports: [
+    PlanificacionModule
   ]
 })
 export class ViewsModule { }
