9440f21335c107e4853b1e69be365c2d463fe3a9
Jaume Pons Orti <jaumeponsorti@gmail.com>
2023-05-29 10:18:35 +0200

    Creado modulo y componente crud

---
+++ src/app/core/shared/views/crud/crud.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud',
+  templateUrl: './crud.component.html',
+  styleUrls: ['./crud.component.scss'],
+})
+export class CrudComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
