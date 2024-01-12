07669d1d472e0b9def9a30071d89316dd1d0ba97
Jaume Pons <43333015+Solojaume@users.noreply.github.com>
2024-01-12 12:42:45 +0100

    limpiado de sidebar

--- projects/super-lib/src/lib/modulos/Super-Sidebars/super-sidebar-avierta/super-sidebar-avierta.component.ts
+++ projects/super-lib/src/lib/modulos/Super-Sidebars/super-sidebar-avierta/super-sidebar-avierta.component.ts
@@ -1,9 +1,7 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Enlace_Menu } from '../../menus/models/enlace_menu';
-import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
-import { Configuracion_Autocompletar } from '../../inputs/modelos/clases/configuracion_autocompletar';
-import { Observable, Subscriber, map, pipe, startWith } from 'rxjs';
+import { Observable} from 'rxjs';
 import { Config_Search } from '../../busquedas/models/Config_Search';
 import { SuperSidebarService } from '../../../servicios/super-sidebar.service';
 import { Usuario } from 'src/app/core/shared/models/usuario';
@@ -13,14 +11,42 @@ import { Usuario } from 'src/app/core/shared/models/usuario';
   templateUrl: './super-sidebar-avierta.component.html',
   styleUrls: ['./super-sidebar-avierta.component.scss']
 })
-export class SuperSidebarAviertaComponent implements OnInit,OnChanges {
+export class SuperSidebarAviertaComponent implements OnChanges {
   constructor(public sidebarService: SuperSidebarService){
   }
+
+  @Input() config!: ConfigSidebar;
+  @Input() enlaces: Enlace_Menu[] = [];
+  @Input () enlaces_perfil!:  Enlace_Menu[];
+  @Input() menuService!:any;
+  @Output() singOutClick: EventEmitter<any> = new EventEmitter();
+  @Input() user!:Usuario;
+
   iniciado: boolean = false;
+  formControl: FormControl = new FormControl("");
+  configBusquedas: Config_Search = 
+  {
+    nombre_campo:"enlaceMENU",
+    nombre_visible:"Busqueda automatica ...",
+    tipo_input:"text",
+    campo_mostrar:{
+      nombre_campo:"nombre_del_campo",
+      nombre_tabla:"unidad_medida",
+    },
+    search_type: "simple_search"
+  };
+  
+  provisonal_enlace_var!:Observable<Enlace_Menu[]>;
+  enlace_menu_resultantes: Observable<Enlace_Menu[]> = new Observable<Enlace_Menu[]>((subscriber)=> {
+    subscriber.next(this.enlaces);
+  });
   
   ngOnChanges(changes: SimpleChanges): void {
-    console.log("ENLACES:", this.enlaces);
-    if(this.iniciado === false){
+    console.log("ENLACES EN SIDEBAR", this.enlaces);
+   
+   
+    if(this.iniciado === false && this.enlaces.length>0){
+      console.log("ENLACES EN SIDEBAR", this.enlaces);
       this.formControl.valueChanges.subscribe(value => { 
         let asignable = JSON.parse(value)
         console.log("Parsed:",asignable);
@@ -44,39 +70,16 @@ export class SuperSidebarAviertaComponent implements OnInit,OnChanges {
     }
   }
 
-  ngOnInit(): void {
-    
-   
-    
-  }
-
   searchClicked(): void {
     if(this.formControl.getRawValue()!==""){
       this.enlace_menu_resultantes = this.provisonal_enlace_var;
-    }
-    
+    }  
   }
-  provisonal_enlace_var!:Observable<Enlace_Menu[]>;
-  enlace_menu_resultantes: Observable<Enlace_Menu[]> = new Observable<Enlace_Menu[]>((subscriber)=> {
-    subscriber.next(this.enlaces);
-  });
-  @Input() config!: ConfigSidebar;
-  @Input() enlaces: Enlace_Menu[] = [];
-  @Input () enlaces_perfil!:  Enlace_Menu[];
-  @Output() singOutClick: EventEmitter<any> = new EventEmitter();
-  @Input() user!:Usuario;
-  formControl: FormControl = new FormControl("");
-  configBusquedas: Config_Search = 
-  {
-    nombre_campo:"enlaceMENU",
-    nombre_visible:"Busqueda automatica ...",
-    tipo_input:"text",
-    campo_mostrar:{
-      nombre_campo:"nombre_del_campo",
-      nombre_tabla:"unidad_medida",
-    },
-    search_type: "simple_search"
-  };
+
+ 
+
+
+ 
   singOut(){
     this.singOutClick.emit("");
   }
@@ -86,8 +89,10 @@ export class SuperSidebarAviertaComponent implements OnInit,OnChanges {
 export interface ConfigSidebar {
   tipo: string;
   tema: string;
+  brandName: string;
   usuario?: User;
   barraBusqueda?: Config_Search;
+  
 }
 
 export interface User{
